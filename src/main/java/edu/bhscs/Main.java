9263e1bbfb33b44eb399e9e731a45e18676dd9c7
Eshaal200 <2001656@apps.nsd.org>
2025-09-12 16:46:32 +0000

    I am mastering println

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,20 @@
-// Student name
-// class period
-// project name
-// date
+// Eshaal Khan
+// P2
+// Zero-hero
+// 9/12/2025
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr. Reiber is saying
+ * INPUT: We don't know about input yet
+ * OUTPUT: Some cool words in the command line terminal thing
+ * EDGE CASE: they all are all edge cases
  */
 
 package edu.bhscs;
+class Main{
+  public static void main(String[] args){
+    System.out.println("Hello");
+    System.out.println("!!!! PROGRAM START !!!!");
+    System.out.println("--------------------------");
+  }
+}
