9e4ece0722be7ca316d6ac3045a4f66f88783851
Eshaal200 <2001656@apps.nsd.org>
2025-09-12 16:57:57 +0000

    Now do I understand commits?

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -15,6 +15,7 @@ class Main{
   public static void main(String[] args){
     System.out.println("--------------------------");
     System.out.println("!!!! PROGRAM START !!!!");
+    System.out.println("");
     System.out.println("Hello");
     System.out.println("!!!!PROGRAM END!!!!");
     System.out.println("--------------------------");
