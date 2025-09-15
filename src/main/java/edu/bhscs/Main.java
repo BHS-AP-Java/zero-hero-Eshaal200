f6183d2577205778b7fb4df6d05d2d5e28d64f61
Eshaal200 <2001656@apps.nsd.org>
2025-09-15 17:06:54 +0000

    For one, what I have been told/observed is if you are out of the file, doing the manual run for terminal will not work but the run button will. someNumber = 67 and A  = a, this was a show of addition because the terminal made "67a!" so then I decided to try fancier ways of doing addition on a computer and added 2 really simple float statements, and I have concluded that if this computer was a human, its an unstoppable calculator.  the lowercase a kinda seperates someNumber from the other numbers. A = a seems kinda weird to me cuz it doesnt really do anything for the code.

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,25 +1,52 @@
 // Eshaal Khan
 // P2
 // Zero-hero
-// 9/12/2025
+// 9/15/2025
 
 /*
- * DESCRIPTION: Whatever Mr. Reiber is saying
+ * DESCRIPTION: Finding something teresting in this code
  * INPUT: We don't know about input yet
  * OUTPUT: Some cool words in the command line terminal thing
  * EDGE CASE: they all are all edge cases
  */
+// this is an example of 'char' constant
+// because it is global, it should be capitalised
 
 package edu.bhscs;
-class Main{
-  public static void main(String[] args){
-    System.out.println("--------------------------");
-    System.out.println("!!!! PROGRAM START !!!!");
+
+class Main {
+
+  public static final char A = 'a';
+  public static void main(String[] args) {
+    int someNumber = 67;
+    System.out.println("67!" + A + someNumber);
+    System.out.println(A + "?" + A + someNumber);
+    System.out.println("A + A + someNumber");
     System.out.println("");
-    System.out.println("Hello");
-    System.out.println("!!!!PROGRAM END!!!!");
-    System.out.println("--------------------------");
-    System.out.println("HEHEHEHEHEHHEHEHEHEHEHEHEHEHEHEHEHEHEHEHE");
-    System.out.println("I feel great");
+    System.out.println("this is neet");
+    System.out.println("");
+    System.out.println("");
+     int funnyInterger = 21;
+
+  System.out.println( funnyInterger + A + someNumber);
+  // thats one fancy way to do some addition
+  //lets see another fancy way
+
+  float funnyNumbers = 21 + 87;
+  float thisjokeisOld = 69 * 69;
+  System.out.println();
+  System.out.println(funnyNumbers + thisjokeisOld + A + someNumber + A + funnyInterger);
+  System.out.println();
+
+  System.out.println("If the computer was human, it would be a human calculator :))))");
+
+
+
+
+  System.out.println("OK I am done");
+
+
+
+
   }
 }
