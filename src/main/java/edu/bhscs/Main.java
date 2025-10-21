5d5a2e2d05321be03ca6e1f0a122d7e67f6da92d
Eshaal200 <2001656@apps.nsd.org>
2025-10-21 03:10:26 +0000

    Assignment Varribles: Replacement is the gate away to abstractions (Main.java)

---
+++ src/main/java/edu/bhscs/Main.java
@@ -0,0 +1,31 @@
+package edu.bhscs;
+
+public class Main {
+
+  public static final char A = 'a';
+  public static void main(String [] args){
+
+
+    int someNumber = 67;
+    int funnyNumbers = 21;
+
+    System.out.println("Lets print some numbers!");
+    System.out.println(A + " ////? " + someNumber + " hmmmm " + funnyNumbers);
+    System.out.println( A + someNumber + A + "!");
+    System.out.println("");
+    System.out.println("");
+    System.out.println(someNumber + funnyNumbers);
+    System.out.println(someNumber/funnyNumbers);
+    System.out.println(someNumber * funnyNumbers);
+    System.out.println("");
+    System.out.println("");
+    System.out.println("Okay im done for now");
+    System.out.println("");
+
+
+
+
+
+  }
+
+}
