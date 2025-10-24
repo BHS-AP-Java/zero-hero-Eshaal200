3546963f1460fbd34eac8bdcf95a1895cf06bdf1
Eshaal200 <2001656@apps.nsd.org>
2025-10-24 05:43:18 +0000

    Assignment, Iteration with objects--- I fixed the pernament fields (flavor and icing) and also fixed the height to shrink when eating a slice!

--- src/main/java/edu/bhscs/App.java
+++ src/main/java/edu/bhscs/App.java
@@ -1,5 +1,7 @@
 package edu.bhscs;
 
+import java.util.Scanner;
+
 public class App {
 
   // Eshaal Khan
@@ -8,17 +10,18 @@ public class App {
   // 10/22/2025
 
   /*
-   * DESCRIPTION: the goal of my code today is to make a cake for name and age
-   * INPUT: typing out name and age, and then drawing a cake for them
+   * DESCRIPTION: the goal of my code today is to make a cake for name and age and flavor and icing
+   * you can eat some slices and it will shrink the cake
+   * INPUT: typing out name, age, flavor, and icing and then drawing a cake for them
    * OUTPUT: a cake with a certain number of candles on it will be drawn
    * EDGE CASE: the candle count is based on the age number
    */
 
   public static void main(String[] args) {
-    date_10_21();
+    date_10_23();
   }
 
-  public static void date_10_21() {
+  public static void date_10_23() {
 
     // fields and properties
 
@@ -26,7 +29,11 @@ public class App {
 
     // constructors
     String name = "Eshaal";
-    String ageInput = "9";
+    String ageInput = "7";
+    String flavor = "chocolate";
+    String icing = "vanilla";
+
+    // flavors you can do: vanilla, strawberry, red velvet, mint ---- same for icing
 
     // constructors
 
@@ -39,7 +46,35 @@ public class App {
       System.out.println("Thats not a number! age automatically a 0.........baby...");
     }
     // methods
-    Cake myCake = new Cake(name, age);
-    myCake.Cakedraw("---------------", "///////////////");
+    Cake myCake = new Cake(name, age, flavor, icing);
+    int slicesEaten = 0;
+    int fullHeight = 7;
+
+    myCake.Cakedraw(slicesEaten);
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (slicesEaten < fullHeight) {
+      System.out.print("\n press enter to eat a slice...");
+      scanner.nextLine();
+      slicesEaten++;
+
+// slices getting eaten
+
+    if (slicesEaten >= fullHeight){
+      System.out.println("\nOh no! You ate all the cake");
+      myCake.Cakedraw(fullHeight);
+      break;
+
+
+    }
+    else{
+      myCake.Cakedraw(slicesEaten);
+    }
+
   }
+
+  scanner.close();
+
+}
 }
--- src/main/java/edu/bhscs/Cake.java
+++ src/main/java/edu/bhscs/Cake.java
@@ -6,21 +6,79 @@ public class Cake {
 
   String name;
   int age;
-  
-// constructor(?)
+  String flavor;
+  String icing;
 
-  public Cake(String name, int age) {
+  // constructor(?)
+
+  public Cake(String name, int age, String flavor, String icing) {
     this.name = name;
     this.age = age;
+    this.flavor = flavor;
+    this.icing = icing;
   }
-
 
   // methods + start of cake drawing
 
-  public void Cakedraw(String x, String y) {
-    int width = 10 + age; // cake width depends on age
-    int height = 4;
-    int candleCount = Math.min(age, 10);
+  public void Cakedraw(int slicesEaten) {
+    int width = 10 + age;
+    int fullHeight = 7;
+    int remainingHeight = Math.max(fullHeight - slicesEaten, 1);
+    int candleCount = Math.min(age, 15);
+
+    // colors!
+
+    // cake flavor/color
+
+    String cakeColor, icingColor;
+    switch ((flavor.toLowerCase())) {
+      case "chocolate":
+        cakeColor = "\u001B[38;2;110;38;14m";
+        break; // brown
+      case "vanilla":
+        cakeColor = "\u001B[37m";
+        break;
+      case "strawberry":
+        cakeColor = "\u001B[35m";
+        break;
+      case "red velvet":
+        cakeColor = "\u001B[31m";
+        break;
+      case "mint":
+        cakeColor = "\u001B[32m";
+        break;
+      default:
+        cakeColor = "\u001B[38;2;110;38;14m"; // brown default
+        break;
+    }
+
+    // icing color
+
+    switch (icing.toLowerCase()) {
+      case "chocolate":
+        icingColor = "\u001B[33m";
+        break;
+      case "vanilla":
+        icingColor = "\u001B[97m";
+        break;
+      case "strawberry":
+        icingColor = "\u001B[95m";
+        break;
+      case "red velvet":
+        icingColor = "\u001B[91m";
+        break;
+      case "mint":
+        icingColor = "\u001B[92m";
+        break;
+      default:
+        icingColor = "\u001B[96m";
+        break;
+    }
+
+    // candle color
+
+    String candleColor = "\u001B[93m"; // bright yellow
+    String reset = "\u001B[0m";
 
     // candle flame *
 
@@ -28,6 +86,7 @@ public class Cake {
       int candleWidth = candleCount * 2 - 1;
       int start = (width - candleWidth) / 2;
 
+      System.out.print(candleColor);
       for (int i = 0; i < width; i++)
         if (i >= start && (i - start) % 2 == 0 && (i - start) / 2 < candleCount)
           System.out.print("*");
@@ -45,39 +104,33 @@ public class Cake {
       System.out.println();
     }
 
+    System.out.println(reset);
+
     // cake body
 
-    for (int row = 0; row < height; row++) {
-      System.out.print("*"); // left edge
+    System.out.print(icingColor);
+    for (int i = 0; i < width; i++) System.out.print("/");
+    System.out.println(reset);
 
-      if (row == 0) {
-        System.out.print(centerTextInLayer(x, width - 2)); // top layer text
-      } else if (row == 1) {
-        System.out.print(centerTextInLayer(y, width - 2)); // second layer text
-      } else {
-        for (int i = 0; i < width - 2; i++) System.out.print(" "); // empty rows
+    for (int row = 0; row < remainingHeight; row++) {
+      System.out.print(cakeColor + "*");
+
+      boolean isIcing = (row % 2 == 1);
+
+      for (int col = 0; col < width - 2; col++) {
+        System.out.print(isIcing ? icingColor + "=" + cakeColor : cakeColor + " ");
       }
-
-      System.out.println("*"); // right edge
+      System.out.println(cakeColor + "*" + reset);
     }
 
-    // cake bottpm
+    // cake bottom
+
+    System.out.print(cakeColor);
 
     for (int i = 0; i < width; i++) System.out.print("*");
+    System.out.print(reset);
     System.out.println();
 
     System.out.println("This cake is for... " + name + " who is " + age + " years old! ");
   }
-
-  public String centerTextInLayer(String text, int layerWidth) {
-    int padding = (layerWidth - text.length()) / 2;
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < padding; i++) sb.append(" ");
-    sb.append(text);
-    for (int i = padding + text.length(); i < layerWidth; i++) sb.append(" ");
-    return sb.toString();
-  }
-
-  // middle of the cake
-
 }
